34e2c1f409d88b0839ffc012f2075329e60a6b03
locpt152 <locpt152@gmail.com>
2022-04-03 19:43:57 +0700

    first commit

--- 60136003_Login/app/src/main/java/a61cntt/a60136003/a60136003_login/HomeActivity.java
+++ 60136003_Login/app/src/main/java/a61cntt/a60136003/a60136003_login/HomeActivity.java
@@ -1,4 +1,17 @@
 package a61cntt.a60136003.a60136003_login;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class HomeActivity {
+public class HomeActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Intent Ihome = getIntent();
+        TextView tv= (TextView) findViewById(R.id.tvusername);
+        tv.setText(String.valueOf(Ihome.getStringExtra("username")));
+    }
 }
--- 60136003_Login/app/src/main/java/a61cntt/a60136003/a60136003_login/LoginActivity.java
+++ 60136003_Login/app/src/main/java/a61cntt/a60136003/a60136003_login/LoginActivity.java
@@ -1,4 +1,43 @@
 package a61cntt.a60136003.a60136003_login;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity {
+
+public class LoginActivity extends AppCompatActivity{
+
+    EditText edtuser, edtpass, edtemail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button btnok = (Button)findViewById(R.id.btnOk);
+        btnok.setOnClickListener(XulysauLogin);
+    }
+
+    View.OnClickListener XulysauLogin = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            edtuser = (EditText)findViewById(R.id.eduser);
+            edtpass = (EditText)findViewById(R.id.edpass);
+            edtemail = (EditText)findViewById(R.id.edemail);
+            String tk = "Phạm Thành Lộc";
+            String mk = "61cntt";
+            String email = "locpt152@gmail.com";
+            String x = edtuser.getText().toString();
+            String y = edtpass.getText().toString();
+            String z = edtemail.getText().toString();
+            if ((x.equals(tk)) && (y.equals(mk)) && (z.equals(email))) {
+                Toast.makeText(LoginActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                Intent Ihome = new Intent(LoginActivity.this, HomeActivity.class);
+                Ihome.putExtra("username", x);
+                startActivity(Ihome);
+            } else
+                Toast.makeText(LoginActivity.this, "Lỗi vui lòng đăng nhập lại !", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
