99f3c15f97237cc3a748d3636df9946b8bd3cbd4
locpt152 <locpt152@gmail.com>
2022-04-05 14:05:24 +0700

    first commit

---
+++ BaiKT2/app/src/main/java/phamthanhloc/baikt2/baikt2/MainActivity.java
@@ -0,0 +1,13 @@
+package phamthanhloc.baikt2.baikt2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
