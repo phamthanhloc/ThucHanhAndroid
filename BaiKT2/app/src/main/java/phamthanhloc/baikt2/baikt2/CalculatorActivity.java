c160b23ba03bb862854cf909bc54fdf712dbb450
locpt152 <locpt152@gmail.com>
2022-04-11 11:42:28 +0700

    first commit

---
+++ BaiKT2/app/src/main/java/phamthanhloc/baikt2/baikt2/CalculatorActivity.java
@@ -0,0 +1,12 @@
+package phamthanhloc.baikt2.baikt2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_calculator);
+    }
+}
--- BaiKT2/app/src/main/java/phamthanhloc/baikt2/baikt2/MainActivity.java
+++ BaiKT2/app/src/main/java/phamthanhloc/baikt2/baikt2/MainActivity.java
@@ -1,13 +1,39 @@
 package phamthanhloc.baikt2.baikt2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtuser, edtpass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnlog = (Button) findViewById(R.id.btnlogin);
+        btnlog.setOnClickListener(XulyLogin);
     }
+
+    View.OnClickListener XulyLogin = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            edtuser = (EditText) findViewById(R.id.eduser);
+            edtpass = (EditText) findViewById(R.id.edpass);
+            String tk = "locpham";
+            String mk = "123";
+            String a = edtuser.getText().toString();
+            String b = edtpass.getText().toString();
+            if((a.equals(tk)) && (b.equals(mk))) {
+                Toast.makeText(MainActivity.this, "User and Password is correct", Toast.LENGTH_SHORT).show();
+                Intent Itip = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(Itip);
+            }
+            else
+                Toast.makeText(MainActivity.this, "User and Password is wrong", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
